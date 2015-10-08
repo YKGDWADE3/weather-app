005959cbc3f4b9e4de40045e1a3c94be58e25612
YKGDWADE3 <superwadeg3@gmail.com>
2015-10-08 21:37:13 +0800

    第一阶段，建立数据库并编写添加的代码

---
+++ CoolWeather/app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,28 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2015/10/8.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return this.provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode(){
+        return this.provinceCode;
+    }
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
